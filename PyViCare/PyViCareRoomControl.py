70227bb4b840dfdb309d3dd95d3daae748702144
Pascâl Hartmann <28061988+LoPablo@users.noreply.github.com>
2023-11-06 09:53:37 +0100

    Chnaged RoomControl Syntax

--- PyViCare/PyViCareRoomControl.py
+++ PyViCare/PyViCareRoomControl.py
@@ -35,10 +35,6 @@ class Room(Device):
         return self.service.getProperty("rooms." + self.id + ".sensors.humidity")["properties"]["value"]["value"]
 
     @handleNotSupported
-    def getHumidity(self):
-        return self.service.getProperty("rooms." + self.id + ".sensors.humidity")["properties"]["value"]["value"]
-
-    @handleNotSupported
     def getOperatingStateReason(self):
         return self.service.getProperty("rooms." + self.id + ".operating.state")["properties"]["reason"]["value"]
 
@@ -46,22 +42,35 @@ class Room(Device):
     def getOperatingStateDemand(self):
         return self.service.getProperty("rooms." + self.id + ".operating.state")["properties"]["demand"]["value"]
 
+    @handleNotSupported
+    def getDesiredTemperatureForProgram(self, program):
+        return \
+            self.service.getProperty(f"rooms.{self.id}.operating.programs.{program}")["properties"][
+                "temperature"]["value"]
+
+    @handleNotSupported
+    def getCurrentDesiredTemperature(self):
+        active_program = self.getActiveProgram()
+        if active_program in ['standby']:
+            return None
+        return self.service.getProperty(f"rooms.{self.id}.operating.programs.{active_program}")[
+            "properties"]["temperature"]["value"]
 
     @handleNotSupported
     def getActiveProgram(self):
-        return self.service.getProperty("rooms." + self.id + ".operating.programs.active")["properties"]["value"]["value"]
+        return self.service.getProperty(f"rooms.{self.id}.operating.programs.active")["properties"]["value"]["value"]
 
-    @handleNotSupported
-    def getTemperatureProgramNormal(self):
-        return self.service.getProperty("rooms." + self.id + ".temperature.levels.normal.perceived")["properties"]["temperature"][
-            "value"]
-
-    @handleNotSupported
-    def getTemperatureProgramComfort(self):
-        return self.service.getProperty("rooms." + self.id + ".temperature.levels.comfort.perceived")["properties"]["temperature"][
-            "value"]
-
-    @handleNotSupported
-    def getTemperatureProgramReduced(self):
-        return self.service.getProperty("rooms." + self.id + ".temperature.levels.reduced.perceived")["properties"]["temperature"][
-            "value"]
+    # @handleNotSupported
+    # def getTemperatureProgramNormal(self):
+    #     return self.service.getProperty("rooms." + self.id + ".temperature.levels.normal.perceived")["properties"]["temperature"][
+    #         "value"]
+    #
+    # @handleNotSupported
+    # def getTemperatureProgramComfort(self):
+    #     return self.service.getProperty("rooms." + self.id + ".temperature.levels.comfort.perceived")["properties"]["temperature"][
+    #         "value"]
+    #
+    # @handleNotSupported
+    # def getTemperatureProgramReduced(self):
+    #     return self.service.getProperty("rooms." + self.id + ".temperature.levels.reduced.perceived")["properties"]["temperature"][
+    #         "value"]
